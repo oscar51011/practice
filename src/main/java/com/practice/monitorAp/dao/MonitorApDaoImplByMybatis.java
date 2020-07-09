b6a1c3c0919c65800c115a118844347ea183b5c9
oscar51011 <oscar51011@gmail.com>
2020-07-10 01:14:50 +0800

    撰寫service測試 & UnitTest，預期拋出 exception的部分 無法直覺撰寫 還在研究

--- src/main/java/com/practice/monitorAp/dao/MonitorApDaoImplByMybatis.java
+++ src/main/java/com/practice/monitorAp/dao/MonitorApDaoImplByMybatis.java
@@ -1,13 +1,19 @@
 package com.practice.monitorAp.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.practice.mapper.MonitorApMapper;
 
 @Repository("monitorApDaoImplByMybatis")
 public class MonitorApDaoImplByMybatis implements MonitorApDao {
 
+	@Autowired
+	private MonitorApMapper mapper;
+	
 	@Override
 	public int getDbConnection() {
-		return 1;	
+		return mapper.getDbConnection();	
 	}
 		
 }
--- src/test/java/com/practice/monitorAp/service/MonitorApServiceTest.java
+++ src/test/java/com/practice/monitorAp/service/MonitorApServiceTest.java
@@ -1,25 +1,78 @@
 package com.practice.monitorAp.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.practice.PracticeApplication;
+import com.practice.monitorAp.dao.MonitorApDao;
 
 @SpringBootTest(classes = {PracticeApplication.class})
 class MonitorApServiceTest {
+	
+	@Mock
+	MonitorApDao dao;
+	
+	@InjectMocks
+	MonitorApService service;
+	
+	@BeforeEach
+	public void init() {
+		// 將參數預設成  env 環境
+		ReflectionTestUtils.setField(service, "apEnviroment", "env");
+		MockitoAnnotations.initMocks(this);
+	}
 
 	// TODO: 後續補齊
-	//@Test
+	@Test
 	void testGetConnectionInfo_ConnectionSuccess() {
-		fail("Not yet implemented");
+		
+		Map<String, String> expected = new HashMap<String, String>();
+		expected.put("enviroment", "env");
+		expected.put("dbConnection", "SUCCESS");
+		
+		when(dao.getDbConnection()).thenReturn(1);
+		
+		Map<String, String> actual = service.getConnectionInfo();
+		
+		assertEquals(expected, actual);
 	}
 	
-	// TODO: 後續補齊
-	//@Tesyt
-	void testGetConnectionInfo_ConnectionFailed() {
-		fail("Not yet implemented");
+	@Test
+	void testGetConnectionInfo_ConnectionReturnNotSuccess() {
+		Map<String, String> expected = new HashMap<String, String>();
+		expected.put("enviroment", "env");
+		expected.put("dbConnection", "FAILED");
+		
+		when(dao.getDbConnection()).thenReturn(2);
+		
+		Map<String, String> actual = service.getConnectionInfo();
+		
+		assertEquals(expected, actual);
+	}
+	
+	//@Test
+	// TODO: 如何預期讓 mockito 模擬錯誤 ?
+	// 還是其實不應該拋 Exception 有待商榷
+	void testGetConnectionInfo_ConnectionThrowException() {
+		Map<String, String> expected = new HashMap<String, String>();
+		expected.put("enviroment", "env");
+		expected.put("dbConnection", "FAILED");
+				
+		Map<String, String> actual = service.getConnectionInfo();
+		
+		assertEquals(expected, actual);
 	}
 
 }
