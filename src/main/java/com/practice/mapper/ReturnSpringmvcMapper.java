4b6527872dad678f9aa7648bd9a3580935de9ec2
oscar51011 <oscar51011@gmail.com>
2020-08-02 01:58:45 +0800

    撰寫 springmvc 練習用的 person 存取物件 dao & unit test

---
+++ src/main/java/com/practice/mapper/ReturnSpringmvcMapper.java
@@ -0,0 +1,13 @@
+package com.practice.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.practice.springmvc.model.PersonModel;
+
+@Repository
+@Mapper
+public interface ReturnSpringmvcMapper {
+	PersonModel getPersonById(@Param("id")int id);
+}
---
+++ src/main/java/com/practice/springmvc/dao/ReturnSpringmvcDao.java
@@ -0,0 +1,7 @@
+package com.practice.springmvc.dao;
+
+import com.practice.springmvc.model.PersonModel;
+
+public interface ReturnSpringmvcDao {
+	public PersonModel getPersonById(int id);
+}
