664035d3e7a61b752ea3447a83e7c09c0b49d974
oscar51011 <oscar51011@gmail.com>
2020-09-07 00:37:37 +0800

    split controller and resrController

---
+++ src/main/java/com/practice/productMaintain/controller/MaintainProductApiController.java
@@ -0,0 +1,37 @@
+package com.practice.productMaintain.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.practice.productMaintain.dto.ProductRequest;
+import com.practice.productMaintain.service.MaintainProductService;
+
+@Controller
+@RequestMapping("/api/maintainProduct")
+public class MaintainProductApiController {
+	
+	private static Logger logger = LoggerFactory.getLogger(MaintainProductController.class);
+	
+	@Autowired
+	private MaintainProductService maintainProductService;
+	
+	@PostMapping(value = "/getProductSkuNumber", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getSkuNameBySkuNumber(@RequestBody ProductRequest product) {
+		
+		logger.info("GetSkuNumber Controller Start, skuNumber is:" + product.getSkuNumber());
+			
+		String name = maintainProductService.getProductNameBySkuNumber(product.getSkuNumber());
+		
+		logger.info("Controller output:" + name);
+		
+		return name;
+	}
+}
