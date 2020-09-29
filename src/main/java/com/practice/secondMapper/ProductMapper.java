ba489b21e8e16651dba6091c5d70718fcdcc3d24
oscar51011 <oscar51011@gmail.com>
2020-09-30 00:36:18 +0800

    use mybatis generator to create second datasource

---
+++ src/main/java/com/practice/secondMapper/ProductMapper.java
@@ -0,0 +1,37 @@
+package com.practice.secondMapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+import com.practice.secnodModel.Product;
+import com.practice.secnodModel.ProductExample;
+
+@Mapper
+public interface ProductMapper {
+    long countByExample(ProductExample example);
+
+    int deleteByExample(ProductExample example);
+
+    int deleteByPrimaryKey(Integer productId);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    List<Product> selectByExampleWithRowbounds(ProductExample example, RowBounds rowBounds);
+
+    List<Product> selectByExample(ProductExample example);
+
+    Product selectByPrimaryKey(Integer productId);
+
+    int updateByExampleSelective(@Param("record") Product record, @Param("example") ProductExample example);
+
+    int updateByExample(@Param("record") Product record, @Param("example") ProductExample example);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
